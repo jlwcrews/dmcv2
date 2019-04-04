3e04134f8a90f22a26ae07ddc2010ae3327db9c0
jlwcrews <jlwcrews@gmail.com>
2019-04-04 22:32:01 +0200

    characters now correctly limited to 6

--- app/src/main/java/no/jlwcrews/dmcv2/views/adapters/CharacterListAdapter.kt
+++ app/src/main/java/no/jlwcrews/dmcv2/views/adapters/CharacterListAdapter.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.recyclerview_character.view.*
 import no.jlwcrews.dmc.db.entities.PlayerCharacter
 import no.jlwcrews.dmcv2.R
 
 class CharacterListAdapter internal constructor(
-    context: Context
+    private val context: Context
 ) : RecyclerView.Adapter<CharacterListAdapter.CharacterViewHolder>() {
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
     private var characters = emptyList<PlayerCharacter>()
+    private var count: Int = 0
+    private val minimumCharacters: Int = 4
+    private val maximumCharacters: Int = 6
+    private var characterList: MutableMap<Int, Boolean> = mutableMapOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val itemView = inflater.inflate(R.layout.recyclerview_character, parent, false)
@@ -25,6 +31,24 @@ class CharacterListAdapter internal constructor(
         val current = characters[position]
         holder.characterNameView.text = current.characterName
         holder.characterRoleView.text = current.characterRole
+
+        holder.itemView.checkBoxCharacter.setOnClickListener{
+            if (holder.itemView.checkBoxCharacter.isChecked){
+                if (count >= maximumCharacters){
+                    holder.itemView.checkBoxCharacter.isChecked = false
+                    Toast.makeText(context, "Maximum of 6 characters.", Toast.LENGTH_SHORT).show()
+                } else{
+                    characterList.set(current.characterId, true)
+                    count++
+                }
+            } else {
+                characterList.set(current.characterId, false)
+                count--
+            }
+            println("Count is $count")
+            characterList.map { println("${it.key} ${it.value}") }
+        }
+
     }
 
     internal fun setCharacters(characters: List<PlayerCharacter>) {
